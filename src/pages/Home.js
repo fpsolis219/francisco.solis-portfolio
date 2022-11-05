0e82733fb40b3648750bdde98a519452dce8ec14
fpsolis219 <fpsolis21995@gmail.com>
2022-11-04 17:07:49 -0700

    initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../components/Header'
+import Footer  from '../components/Footer'
+
+
+const Home = () => {
+  return (
+    <>
+    <Header/>
+    <div>Home</div>
+    <Footer/>
+    </>
+  )
+}
+
+export default Home
