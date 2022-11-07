47afe9bdf2c351895159ee837f998bfd84958fb0
fpsolis219 <fpsolis21995@gmail.com>
2022-11-07 09:49:24 -0800

    more dependencies installed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import Header from '../components/Header'
 import Footer  from '../components/Footer'
-
+// import code from '../assets/code.jpg'
 
 const Home = () => {
-  return (
+    return (
 
     <>
+    <div className='bg-[url(../assets/code.jpg)]'> 
     <Header/>
-    <div>Home</div>
+    <div className='text-center'>
+        <h1 className='text-2xl  underline text-underline-offset-4'>Welcome to my Porfolio!</h1>
+        <p>My name is Francisco Solis and </p>
+        <p>I am an aspiring Full Stack web Developer</p>
+        <p>Here you will find information about me, experience/skills, projects and contact information for potential opportunities</p>
+    </div>
     <Footer/>
+    </div>
     </>
-  )
+    )
 }
 
 
