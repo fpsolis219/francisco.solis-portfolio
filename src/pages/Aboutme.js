8fd4ddde2e0e3d14d5fe8f156f650a7aba0676b0
fpsolis219 <fpsolis21995@gmail.com>
2022-11-17 13:17:52 -0800

    navbar styling

--- src/pages/Aboutme.js
+++ src/pages/Aboutme.js
@@ -7,7 +7,7 @@ const Aboutme = () => {
         <center>About me.</center>
       </div>
       <div>
-          <p> I am a United States Army Veteran with a background of operating various types of motorized aparatuses such as: Bulldozers, Excavator, Compactor, Front-Loader, Backhoe, Dump-Truck etc. For 7 years I have been committed to that profesion, until I decided it was time to make  a drastic change to seek out better oppurtunities. That oppurtunity started with LEARN Academy... <br/><br/> <a href="https://www.learnacademy.org/"> @LEARN Academy</a>, I aquired the skills needed to be software engineer. As a Graduate I continue to practice the fundamentals of web developing and specialize in: Javascript, ReactJS, Ruby, Postgres and Ruby On Rails. </p>
+        <p> I am a United States Army Veteran with a background of operating various types of motorized aparatuses such as: Bulldozers, Excavator, Compactor, Front-Loader, Backhoe, Dump-Truck etc. For 7 years I have been committed to that profesion, until I decided it was time to make  a drastic change to seek out better oppurtunities. That oppurtunity started with LEARN Academy... <br/><br/> <a href="https://www.learnacademy.org/"> @LEARN Academy</a>, I aquired the skills needed to be software engineer. As a Graduate I continue to practice the fundamentals of web developing and specialize in: Javascript, ReactJS, Ruby, Postgres and Ruby On Rails. </p>
       </div>
     </>
   )
