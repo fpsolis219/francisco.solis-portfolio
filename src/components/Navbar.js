110780d927e383e857fcba8e230c7bb14c8fdd44
fpsolis219 <fpsolis21995@gmail.com>
2022-11-17 17:01:05 -0800

    styling, gradiants added.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,14 +8,12 @@ import { Button } from "flowbite-react"
 const Navbar = () => {
   return (
     <>
-      <div className="space-x-4 p-8 border-4 border-gold mx-[60px] my-8 flex flex-auto place-content-evenly"
-        style={ {backgroundImage: "url('https://images.unsplash.com/photo-1586013277290-a94871df7bb0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2787&q=80')"}}>
+      <div className="space-x-4 p-8 border-4 border-black flex flex-auto place-content-evenly bg-slate-700 bg-gradient-to-r from-red-800">
 
-        <Button className="bg-slate-800 hover:bg-slate-600"><a href="/experience" className="underline underline-offset-8"> About me </a></Button>
-        <Button className="bg-slate-800 hover:bg-slate-600"><a href="/experience" className="underline underline-offset-8">Experience/skills</a></Button>
-        <Button className="bg-slate-800 hover:bg-slate-600"><a href="/projects" className="underline underline-offset-8">Projects</a></Button>
-        <Button className="bg-slate-800 hover:bg-slate-600"><a href="/interests" className="underline underline-offset-8">Interests</a></Button>
-        <Button className="bg-slate-800 hover:bg-slate-600"><a href="/contact" className="underline underline-offset-8">Contact Information</a></Button>
+        <Button className="bg-slate-600 hover:bg-slate-700 shadow-2xl"><a href="/experience" className="underline underline-offset-8"> About me </a></Button>
+        <Button className="bg-slate-600 hover:bg-slate-700 shadow-2xl"><a href="/projects" className="underline underline-offset-8">Projects</a></Button>
+        <Button className="bg-slate-600 hover:bg-slate-700 shadow-2xl"><a href="/interests" className="underline underline-offset-8">Interests</a></Button>
+        <Button className="bg-slate-600 hover:bg-slate-700 shadow-2xl"><a href="/contact" className="underline underline-offset-8">Contact Information</a></Button>
           
       </div>
       
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,31 +12,33 @@ const Home = () => {
     
 
     <>
-      <div className="bg-cover text-center text-gold text-2xl font-serif h-auto w-auto selection:bg-red-800 bg-slate-300 border-[30px] border-red-800 border-opacity-75 " > 
-        <div className="bg-contain p-8 mx-[60px] border-4 border-gold my-8"
-          style={ {backgroundImage: "url('https://images.unsplash.com/photo-1586013277290-a94871df7bb0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2787&q=80')"}} 
-        > 
-        
-          {/* <Header/> */}
+      <div className="text-center text-gold text-2xl font-serif selection:bg-red-800 bg-slate-700 bg-gradient-to-r from-red-800" > 
+        <div className="bg-cover h-[550px]" style={ {backgroundImage: "url('https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80')"}} >
+          <br/>
+          <br/>
+          <br/>
           <center> 
             <img
-              className="mb-3 h-[170px] w-[170px] rounded-full shadow-lg my-8"
+              className="mb-3 h-[170px] w-[170px] rounded-full shadow-lg"
               src="https://media-exp1.licdn.com/dms/image/sync/C5627AQFh79ey-e1P0g/articleshare-shrink_480/0/1668314287064?e=1669266000&v=beta&t=QFYQ2Ns6vaRq8FppoVUioZybwhpbveyEw8DIAM6csf0"
               alt="Bonnie image"
             />
           </center>
-          <h1 className="text-underline-offset-4  text-5xl underline my-8"> <mark className="bg-red-800/50 text-gold"> Welcome to my Porfolio!</mark>
+          <h1 className="underline-offset-8  text-5xl underline my-8"> <mark className="bg-red-800/75 text-gold"> Welcome to my Portfolio!</mark>
           </h1>
           <div className="my-8"> 
             <p><mark className="bg-red-800/50 text-gold">My name is Francisco Solis and </mark>
             </p>
+            <br/>
             <p><mark className="bg-red-800/50 text-gold">I am an aspiring Full Stack web Developer.</mark>
             </p>
-            <p><mark className="bg-red-800/50 text-gold">Here you will find information about me, my experience/skills, projects and contact information for potential opportunities</mark>
+            <br/>
+            <p><mark className="bg-red-800/50 text-gold">Get to know me!</mark>
             </p>
           </div>
+          
         </div>
-        <div className="">
+        <div>
           <Navbar/>
         </div>
         <Aboutme/>
