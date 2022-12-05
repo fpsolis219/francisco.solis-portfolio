5598383e2e1aa05837e5da44250b2e4667a7c026
Francisco Solis <fpsolis21995@gmail.com>
2022-12-05 16:06:02 -0600

    added react router dom

---
+++ src/components/ProjectData.js
@@ -0,0 +1,18 @@
+import GRD from "../assets/Gym Rat Deals.png"
+
+
+
+
+
+
+
+const ProjectData = [
+  {
+    id: 1,
+    image: GRD,
+    name: "Gym Rat Deals",
+    description: ,
+    link: "https://gymratdeals.herokuapp.com/"
+  }
+]
+export default ProjectData
